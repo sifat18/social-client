99917bb76dc5c5ecea9cd98622bb8cd4549b6cb5
sifat18 <ibnsifat@gmail.com>
2023-04-28 04:27:57 +0600

    home done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
 import { Profile } from "./pages/profile";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -12,6 +12,8 @@ function App() {
 
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
     <>
       <div className="app">
@@ -20,8 +22,8 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<Login />}></Route>
-            <Route path="/home" element={<Home />}></Route>
-            <Route path="/profile/:userId" element={<Profile />}></Route>
+            <Route path="/home" element={isAuth ? <Home /> : <Navigate to="/" />}></Route>
+            <Route path="/profile/:userId" element={isAuth ?<Profile />: <Navigate to="/" />}></Route>
           </Routes>
           </ThemeProvider>
         </BrowserRouter>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,44 @@
-import { Box } from "@mui/material"
+import { Box, useMediaQuery } from "@mui/material"
 import { Nav } from "../../component/Nav"
+import UserWidget from "../../component/UserWidget";
+import { useSelector } from "react-redux";
+import MyPostWidget from "../../component/MyPostWidget";
+import PostsWidget from "../../component/PostsWidget";
+import AdvertWidget from "../../component/AdvertWidget";
+import FriendListWidget  from "../../component/FriendListWidget";
 // import { Nav } from "../../component/Nav"
 
 export const Home = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picture } = useSelector((state) => state.user);
   return (
     <Box>
       <Nav/>
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picture={picture} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picture} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
+    </Box>
     </Box>
   )
 }
