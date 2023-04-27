f0311ec3e0edfa3caf9509cbbaf397dfc117635a
sifat18 <ibnsifat@gmail.com>
2023-04-27 16:27:40 +0600

    redux and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { Home } from "./pages/home"
+import { Login } from "./pages/login"
+import { Profile } from "./pages/profile"
+import { BrowserRouter,Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <div>
-      <h2>socio</h2>
+      <div className="app">
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}></Route>
+          <Route path='/home' element={<Home/>}></Route>
+          <Route path='/profile/:userId' element={<Profile/>}></Route>
+        </Routes>
+        </BrowserRouter>
+      
+      
       </div>
      
     </>
